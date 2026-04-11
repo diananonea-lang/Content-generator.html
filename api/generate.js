b74158006d14ef9890b83dd0ec8463dec6e11bff
diananonea-lang <diananonea@gmail.com>
2026-04-11 19:38:04 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,6 @@
-export const config = { runtime: 'edge' };
 
-export default async function handler(req) {
+
+export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return new Response(null, {
       headers: {
